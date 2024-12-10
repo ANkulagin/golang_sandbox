e36887f3c53fae9eb48dada288c3f540efda38f3
Andrey Kulagin <ankulskull@gmail.com>
2024-12-10 18:57:52 +0300

    Added articles_and_books/100_go_mistakes/code_and_project_organization/2_error

--- articles_and_books/100_go_mistakes/code_and_project_organization/2_error/correction/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/2_error/correction/main.go
@@ -10,22 +10,22 @@ import (
 func join(s1, s2 string, max int) (string, error) {
 	if s1 == "" {
 		return "", errors.New("s1 is empty")
-	} else {
-		if s2 == "" {
-			return "", errors.New("s2 is empty")
-		} else {
-			concat, err := concatenate(s1, s2) // Вызов функции concatenate
-			if err != nil {
-				return "", err
-			} else {
-				if len(concat) > max {
-					return concat[:max], nil
-				} else {
-					return concat, nil
-				}
-			}
-		}
 	}
+
+	if s2 == "" {
+		return "", errors.New("s2 is empty")
+	}
+
+	concat, err := concatenate(s1, s2) // Вызов функции concatenate
+	if err != nil {
+		return "", err
+	}
+
+	if len(concat) > max {
+		return concat[:max], nil
+	}
+
+	return concat, nil
 }
 
 // Вспомогательная функция для конкатенации строк
