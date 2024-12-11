e303c2e7a2ea9237992bfd5f9a843c019554629a
Andrey Kulagin <ankulskull@gmail.com>
2024-12-11 16:32:18 +0300

    Added articles_and_books/100_go_mistakes/code_and_project_organization/3_error

--- articles_and_books/100_go_mistakes/code_and_project_organization/3_error/correction/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/3_error/correction/main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+// Функция для создания и инициализации подключения к базе данных
+func createDB(dataSourceName string) (*sql.DB, error) {
+	// Открытие соединения с базой данных
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось открыть соединение с базой данных: %w", err)
+	}
+
+	// Проверка доступности базы данных
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("не удалось подключиться к базе данных: %w", err)
+	}
+
+	return db, nil
+}
+
+func main() {
+	// Получение строки подключения из переменной окружения
+	dataSourceName := os.Getenv("MYSQL_DATA_SOURCE_NAME")
+	if dataSourceName == "" {
+		log.Fatal("переменная окружения MYSQL_DATA_SOURCE_NAME не установлена")
+	}
+
+	// Инициализация подключения к базе данных
+	db, err := createDB(dataSourceName)
+	if err != nil {
+		log.Fatalf("ошибка инициализации базы данных: %v", err)
+	}
+	defer db.Close() // Закрытие соединения при завершении программы
+
+	// Использование инициализированного подключения
+	err = db.Ping()
+	if err != nil {
+		log.Println("Ошибка подключения к базе данных:", err)
+		return
+	}
+	log.Println("Подключение к базе данных успешно")
+}
--- articles_and_books/100_go_mistakes/code_and_project_organization/3_error/error/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/3_error/error/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+// Функция инициализации для открытия базы данных
+func init() {
+	// Получение строки подключения из переменной окружения
+	dataSourceName := os.Getenv("MYSQL_DATA_SOURCE_NAME")
+	d, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Panic(err) // Паника при ошибке открытия базы данных
+	}
+	err = d.Ping()
+	if err != nil {
+		log.Panic(err) // Паника при ошибке пинга базы данных
+	}
+	db = d // Присвоение глобальной переменной db
+}
+
+func main() {
+	// Использование глобальной переменной db
+	// Пример запроса к базе данных
+	err := db.Ping()
+	if err != nil {
+		log.Println("Ошибка подключения к базе данных:", err)
+		return
+	}
+	log.Println("Подключение к базе данных успешно")
+}
