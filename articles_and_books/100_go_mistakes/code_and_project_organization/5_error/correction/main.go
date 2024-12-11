0f3951e3a1a9449883d50a115d1e3d6d1d715a89
Andrey Kulagin <ankulskull@gmail.com>
2024-12-11 20:13:42 +0300

    Added articles_and_books/100_go_mistakes/code_and_project_organization/5_error

--- articles_and_books/100_go_mistakes/code_and_project_organization/5_error/correction/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/5_error/correction/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+// Структура Customer с экспортированным полем Balance
+type Customer struct {
+	Balance float64
+}
+
+func main() {
+	customer := &Customer{}
+
+	// Прямой доступ к полю Balance для установки значения
+	customer.Balance = 200.0
+
+	// Прямой доступ к полю Balance для получения значения
+	fmt.Printf("Текущий баланс: %.2f\n", customer.Balance)
+
+	// Прямой доступ к полю Balance для обнуления
+	customer.Balance = 0
+	fmt.Printf("Баланс после обнуления: %.2f\n", customer.Balance)
+}
--- articles_and_books/100_go_mistakes/code_and_project_organization/5_error/error/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/5_error/error/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+)
+
+// Интерфейс для получения баланса
+type BalanceGetter interface {
+	GetBalance() float64
+}
+
+// Интерфейс для установки баланса
+type BalanceSetter interface {
+	SetBalance(amount float64)
+}
+
+// Структура Customer с не экспортированным полем balance
+type Customer struct {
+	balance float64
+}
+
+// Реализация метода GetBalance
+func (c *Customer) GetBalance() float64 {
+	return c.balance
+}
+
+// Реализация метода SetBalance
+func (c *Customer) SetBalance(amount float64) {
+	c.balance = amount
+}
+
+func main() {
+	// Использование интерфейсов для доступа к полям
+	var getter BalanceGetter = &Customer{}
+	var setter BalanceSetter = &Customer{}
+
+	// Установка баланса через сеттер
+	setter.SetBalance(150.0)
+
+	// Получение баланса через геттер
+	currentBalance := getter.GetBalance()
+	fmt.Printf("Текущий баланс: %.2f\n", currentBalance)
+}
