7563f2cfdf705fc6ba3f3c913ab0a1dbe1827388
Andrey Kulagin <ankulskull@gmail.com>
2024-12-11 17:23:34 +0300

    Added articles_and_books/100_go_mistakes/code_and_project_organization/4_error

--- articles_and_books/100_go_mistakes/code_and_project_organization/4_error/correction/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/4_error/correction/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+)
+
+// Экспортированное поле Balance
+type Customer struct {
+	Balance float64
+}
+
+func main() {
+	customer := &Customer{}
+
+	// Прямой доступ к полю Balance для установки значения
+	customer.Balance = 100.0
+
+	// Прямой доступ к полю Balance для получения значения
+	fmt.Printf("Текущий баланс: %.2f\n", customer.Balance)
+
+	// Прямой доступ к полю Balance для обнуления
+	customer.Balance = 0
+	fmt.Printf("Баланс после обнуления: %.2f\n", customer.Balance)
+}
--- articles_and_books/100_go_mistakes/code_and_project_organization/4_error/error/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/4_error/error/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+)
+
+// Структура Customer с неэкспортированным полем balance
+type Customer struct {
+	balance float64
+}
+
+// Геттер для поля balance
+func (c *Customer) Balance() float64 {
+	return c.balance
+}
+
+// Сеттер для поля balance
+func (c *Customer) SetBalance(amount float64) {
+	c.balance = amount
+}
+
+func main() {
+	customer := &Customer{}
+
+	// Установка баланса через сеттер
+	customer.SetBalance(100.0)
+
+	// Получение баланса через геттер
+	currentBalance := customer.Balance()
+	fmt.Printf("Текущий баланс: %.2f\n", currentBalance)
+
+	// Обнуление баланса через сеттер
+	customer.SetBalance(0)
+	fmt.Printf("Баланс после обнуления: %.2f\n", customer.Balance())
+}
