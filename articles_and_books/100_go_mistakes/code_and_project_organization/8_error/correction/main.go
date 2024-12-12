81667b018710abe34e9f873b0682b25a7228147f
Andrey Kulagin <ankulskull@gmail.com>
2024-12-12 17:02:37 +0300

    Added articles_and_books/100_go_mistakes/code_and_project_organization/8_error

--- articles_and_books/100_go_mistakes/code_and_project_organization/8_error/correction/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/8_error/correction/main.go
@@ -1,1 +1,40 @@
-package main
+package store
+
+// Структура Customer
+type Customer struct {
+	ID      string
+	Balance float64
+}
+
+// Структура Contract
+type Contract struct {
+	ID     string
+	Active bool
+}
+
+// Структура Store
+type Store struct{}
+
+// Метод GetCustomer возвращает конкретный тип Customer
+func (s *Store) GetCustomer(id string) (Customer, error) {
+	// Логика получения клиента
+	return Customer{}, nil
+}
+
+// Метод SetCustomer принимает конкретный тип Customer
+func (s *Store) SetCustomer(id string, customer Customer) error {
+	// Логика установки клиента
+	return nil
+}
+
+// Метод GetContract возвращает конкретный тип Contract
+func (s *Store) GetContract(id string) (Contract, error) {
+	// Логика получения контракта
+	return Contract{}, nil
+}
+
+// Метод SetContract принимает конкретный тип Contract
+func (s *Store) SetContract(id string, contract Contract) error {
+	// Логика установки контракта
+	return nil
+}
--- articles_and_books/100_go_mistakes/code_and_project_organization/8_error/error/main.go
+++ articles_and_books/100_go_mistakes/code_and_project_organization/8_error/error/main.go
@@ -1,1 +1,28 @@
-package main
+package store
+
+// Структура Customer
+type Customer struct {
+	ID      string
+	Balance float64
+}
+
+// Структура Contract
+type Contract struct {
+	ID     string
+	Active bool
+}
+
+// Структура Store
+type Store struct{}
+
+// Метод Get возвращает any
+func (s *Store) Get(id string) (any, error) {
+	// Логика получения данных
+	return nil, nil
+}
+
+// Метод Set принимает any
+func (s *Store) Set(id string, v any) error {
+	// Логика установки данных
+	return nil
+}
